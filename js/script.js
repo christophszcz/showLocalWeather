dde7b2935934dea80778d7fae3f6286ae594185b
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-05 12:43:50 -0500

    The API has been tested and works.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
-
+	$.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=8301741adabddef5ba1d50b386f29c9d", function(json) {
+	  $(".container").html(JSON.stringify(json));
+	});
 });
