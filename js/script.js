ccefc49c4f9159f25d2be9e349b96425af7ea0af
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-06 23:59:38 -0500

    Tested the output.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-	$.getJSON("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=8301741adabddef5ba1d50b386f29c9d", function(json) {
-	  $(".container").html(JSON.stringify(json));
-	});
+$(document).ready(function() {
+
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=8301741adabddef5ba1d50b386f29c9d", function(data) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+ 
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
 });
+
