bde88055d035744d2df7fa60da3d7a03d5513dec
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-07 23:05:19 -0500

    Tested London conditions and output from the API.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
 
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=8301741adabddef5ba1d50b386f29c9d", function(data) {
-    var items = [];
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=8301741adabddef5ba1d50b386f29c9d", function(data) {
+    // var items = [];
     $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
+      current_location =  "<p>" +  data.name + "</p>";
+      conditions =  "<p>" + data.main.temp + "</p>";
+      wind =  "<p>" +  data.wind.speed + " meter/sec</p>";
     });
- 
-    $( "<ul/>", {
-      "class": "my-new-list",
-      html: items.join( "" )
-    }).appendTo( "body" );
+
+    $('.current-location').html(current_location);
+    $('.conditions').html(conditions);
+    $('.wind').html(wind);
   });
 });
 
