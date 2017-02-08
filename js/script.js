a196ec8463c0980bc6c02f7356308c8d7617b1a5
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-08 17:19:20 -0500

    The location of the user is captured via long and lat and shown.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
   
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-      $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&appid=8301741adabddef5ba1d50b386f29c9d", function(data) {
-        // var items = [];
+      $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&units=metric&appid=8301741adabddef5ba1d50b386f29c9d", function(data) {
+        
+        //$('body').html("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&unit=metric&appid=8301741adabddef5ba1d50b386f29c9d");
         $.each( data, function( key, val ) {
-          current_location =  "<p>" +  data.name + "</p>";
-          conditions =  "<p>" + data.main.temp + "</p>";
-          wind =  "<p>" +  data.wind.speed + " meter/sec</p>";
+          current_location =  "<p>" + data.list[0].name + "</p>";
+          conditions =  "<p>" + data.list[0].main.temp + "&deg; C</p>";
+          wind =  "<p>" +  data.list[0].wind.speed + " meter/sec</p>"
         });
 
         $('.current-location').html(current_location);
