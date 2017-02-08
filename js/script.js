5bb4c49baddf5f061100897c707cc43b8ffcc3db
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-07 23:23:27 -0500

    Input the longitude and latitude.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&appid=8301741adabddef5ba1d50b386f29c9d", function(data) {
+        // var items = [];
+        $.each( data, function( key, val ) {
+          current_location =  "<p>" +  data.name + "</p>";
+          conditions =  "<p>" + data.main.temp + "</p>";
+          wind =  "<p>" +  data.wind.speed + " meter/sec</p>";
+        });
 
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=8301741adabddef5ba1d50b386f29c9d", function(data) {
-    // var items = [];
-    $.each( data, function( key, val ) {
-      current_location =  "<p>" +  data.name + "</p>";
-      conditions =  "<p>" + data.main.temp + "</p>";
-      wind =  "<p>" +  data.wind.speed + " meter/sec</p>";
+        $('.current-location').html(current_location);
+        $('.conditions').html(conditions);
+        $('.wind').html(wind);
+      }); 
     });
-
-    $('.current-location').html(current_location);
-    $('.conditions').html(conditions);
-    $('.wind').html(wind);
-  });
+  }  
 });
 
