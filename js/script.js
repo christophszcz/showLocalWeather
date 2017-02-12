3242d2284f9f1af4726aa1082b5ab1d3f22bc41b
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-11 19:18:21 -0500

    You can now toggle between C or F for the button.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ $(document).ready(function() {
 
   $('#change-unit').on('click', function(){
     units = 'imperial';
+    $('.unit-value').toggle();
   });
   
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+    navigator.geolocation.getCurrentPosition(function(position) {
 
       units = 'metric';
       $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&units=" + units + "&appid=8301741adabddef5ba1d50b386f29c9d", function(data) {
@@ -18,6 +19,11 @@ $(document).ready(function() {
           conditions =  "<p>" + data.list[0].main.temp + "&deg; C</p>";
           wind =  "<p>" +  data.list[0].wind.speed + " meter/sec</p>"
         });
+
+        if (previous && current && previous !== current) {
+          console.log('refresh');
+          location.reload();
+        }
 
         $('.current-location').html(current_location);
         $('.conditions').html(conditions);
