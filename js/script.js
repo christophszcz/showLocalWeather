a2809d391fe85620766fb07f3d75dfa3420563db
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-13 00:11:14 -0500

    The temperature is now a rounded number.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         //$('body').html("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&unit=metric&appid=8301741adabddef5ba1d50b386f29c9d");
         $.each( data, function( key, val ) {
           current_location =  "<p>" + data.list[0].name + "</p>";
-          conditions =  "<p>" + data.list[0].main.temp + "&deg; C</p>";
+          conditions =  "<p>" + Math.floor(data.list[0].main.temp) + "&deg; C</p>";
           wind =  "<p>" +  data.list[0].wind.speed + " meter/sec</p>"
         });
 
@@ -18,10 +18,10 @@ $(document).ready(function() {
           units = (units == 'metric') ? 'imperial' : 'metric';
 
           if (units === 'imperial') {
-            var farenheit = "<p>" + (data.list[0].main.temp * (9/5) + 32)  + "&deg; F</p>";
+            var farenheit = "<p>" + Math.floor(data.list[0].main.temp * (9/5) + 32)  + "&deg; F</p>";
             $('.conditions').html(farenheit);
           } else if (units === 'metric') {
-            var celsius = "<p>" + data.list[0].main.temp + "&deg; C</p>";
+            var celsius = "<p>" + Math.floor(data.list[0].main.temp) + "&deg; C</p>";
             $('.conditions').html(celsius);
           }
         });
