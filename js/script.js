a9fdc34d8ad77b01a7c189f0097c90432991d489
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-15 15:12:07 -0500

    Now the actual city name is intercepted via your current IP address.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
   
   if (navigator.geolocation) {
+
+    $.getJSON('http://ip-api.com/json/', function(data) {  //I.P address API
+      //console.log(JSON.stringify(data, null, 2));
+      current_location =  "<p>" + data.city + "</p>";
+      $('.current-location').html(current_location);
+    });
+
     navigator.geolocation.getCurrentPosition(function(position) {
 
       units = 'metric';
-      $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&units=" + units + "&appid=8301741adabddef5ba1d50b386f29c9d", function(data) {
+      $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=1&units=" + units + "&appid=8301741adabddef5ba1d50b386f29c9d", function(data) {
         
         //$('body').html("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&unit=metric&appid=8301741adabddef5ba1d50b386f29c9d");
         $.each( data, function( key, val ) {
-          current_location =  "<p>" + data.list[0].name + "</p>";
           conditions =  "<p>" + Math.floor(data.list[0].main.temp) + "&deg; C</p>";
           wind =  "<p>" +  Math.floor(data.list[0].wind.speed * 3.6) + " km/hr</p>"
         });
@@ -26,7 +32,6 @@ $(document).ready(function() {
           }
         });
 
-        $('.current-location').html(current_location);
         $('.conditions').html(conditions);
         $('.wind').html(wind);
  
