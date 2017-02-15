80be672083e91b3c5afcc0e1b2ad4852a84d969d
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-15 15:28:06 -0500

    Now the current location and weather api are working in tandem.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,34 @@
 $(document).ready(function() {
-  
-  if (navigator.geolocation) {
-
-    $.getJSON('http://ip-api.com/json/', function(data) {  //I.P address API
-      //console.log(JSON.stringify(data, null, 2));
-      current_location =  "<p>" + data.city + "</p>";
-      $('.current-location').html(current_location);
-    });
-
-    navigator.geolocation.getCurrentPosition(function(position) {
-
-      units = 'metric';
-      $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=1&units=" + units + "&appid=8301741adabddef5ba1d50b386f29c9d", function(data) {
-        
-        //$('body').html("http://api.openweathermap.org/data/2.5/find?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=10&unit=metric&appid=8301741adabddef5ba1d50b386f29c9d");
-        $.each( data, function( key, val ) {
-          conditions =  "<p>" + Math.floor(data.list[0].main.temp) + "&deg; C</p>";
-          wind =  "<p>" +  Math.floor(data.list[0].wind.speed * 3.6) + " km/hr</p>"
-        });
-
-        $('#change-unit').on('click', function(){
-          $('.unit-value').toggle();
-          units = (units == 'metric') ? 'imperial' : 'metric';
-
-          if (units === 'imperial') {
-            var farenheit = "<p>" + Math.floor(data.list[0].main.temp * (9/5) + 32)  + "&deg; F</p>";
-            $('.conditions').html(farenheit);
-          } else if (units === 'metric') {
-            var celsius = "<p>" + Math.floor(data.list[0].main.temp) + "&deg; C</p>";
-            $('.conditions').html(celsius);
-          }
-        });
-
-        $('.conditions').html(conditions);
-        $('.wind').html(wind);
  
-      }); 
-    });
-  }
+  $.getJSON('http://ip-api.com/json/', function(data) {  //I.P address API
+    //console.log(JSON.stringify(data, null, 2));
+    current_location =  "<p>" + data.city + "</p>";
+    city_name = data.city
+    $('.current-location').html(current_location);
+    apiKeyString = "&appid=8301741adabddef5ba1d50b386f29c9d";
+    units = 'metric';
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&units=" + units + apiKeyString, function(json) { //Weather API
+      
+      //$('body').html("http://api.openweathermap.org/data/2.5/weather?q=" + city_name + "&units=" + units + apiKeyString);
+      
+      conditions =  "<p>" + Math.floor(json.main.temp) + "&deg; C</p>";
+      wind =  "<p>" +  Math.floor(json.wind.speed * 3.6) + " km/hr</p>";
+
+      $('#change-unit').on('click', function(){
+         $('.unit-value').toggle();
+         units = (units == 'metric') ? 'imperial' : 'metric'
+         if (units === 'imperial') {
+           var farenheit = "<p>" + Math.floor(json.main.temp * (9/5) + 32)  + "&deg; F</p>";
+           $('.conditions').html(farenheit);
+         } else if (units === 'metric') {
+           var celsius = "<p>" + Math.floor(json.main.temp) + "&deg; C</p>";
+           $('.conditions').html(celsius);
+         }
+      });
+
+      $('.conditions').html(conditions);
+      $('.wind').html(wind);
+    }); 
+  });
+
 });
