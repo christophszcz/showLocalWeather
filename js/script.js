80561e368d94a65f95db296694bc8cdd97be1009
christophszcz <christoph.szczechowicz@gmail.com>
2017-02-26 23:45:30 -0500

    Added moment.js library on client side.

--- js/script.js
+++ js/script.js
@@ -58,7 +58,17 @@ $(document).ready(function() {
       var time = currentTime.getTime();
       // $('.time-data').html(currentTime);
 
-      $('.time-data').html(json.dt);
+      var date = new Date(json.dt *1000);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+      $('.time-data').html(formattedTime);
 
       if(json.weather[0].icon == '01d' || json.weather[0].icon == '02d'){
         $('body').css('background', "url('../images/clear_day.jpg')");
